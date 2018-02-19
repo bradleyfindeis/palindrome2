01f9f5acb9cef3b60d8169dc80366f18ca2aa4d8
Bradley Findeis <bradleyfindeis@BradleysMacBook.hsd1.ut.comcast.net>
2018-02-18 17:53:20 -0700

    Getting closer

--- palindrome.rb
+++ palindrome.rb
@@ -1,6 +1,8 @@
 require 'Pry'
 
 def menu
+  orig = []
+  new_arr = []
   puts " "
   puts "------------------"
   puts "------------------"
@@ -12,8 +14,30 @@ def menu
   puts "Enter some text to see if it's a Palindrome (a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run"
   puts "Press '2' to exit"
   @input = gets.strip
-  logic
-end 
+  orig << @input
+
+  letters = orig.split(' ')
+  if letters.length >= 1
+    last_letter = letters.pop
+    new_arr << last_letter
+  else
+    if orig == new_arr
+      puts "Hey, that's a palindrome. Good work."
+      menu
+    else
+      puts "Nope, try agian"
+    end
+  end
+end
+menu
+
+
+ # Create loop
+ # seperate letters in array
+ # Put letters backwards in new array
+ # compare arrays
+
+
 
 # figure out how to do this without using a helper method....
 # the logic should be simple...
@@ -40,5 +64,3 @@ end
 #   end
 #   menu
 # end
-
-menu
